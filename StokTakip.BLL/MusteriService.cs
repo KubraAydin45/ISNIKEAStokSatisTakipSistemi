ebefc8c8c7f547921c7763e1be40a93f2cf8ee8a
Kübra Aydın <kaydiin1905@gmail.com>
2026-01-15 01:42:54 +0300

    Add files via upload

---
+++ StokTakip.BLL/MusteriService.cs
@@ -0,0 +1,31 @@
+﻿using StokTakip.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.BLL
+{
+    public class MusteriService
+    {
+        MusteriDAO musteriDao = new MusteriDAO();
+
+        public List<Musteri> MusterileriListele()
+        {
+            DataTable dt = musteriDao.MusterileriGetir();
+            List<Musteri> liste = new List<Musteri>();
+
+            foreach (DataRow satir in dt.Rows)
+            {
+                liste.Add(new Musteri
+                {
+                    id = Convert.ToInt32(satir["id"]),
+                    MusteriAdSoyad = satir["MusteriAdSoyad"].ToString()
+                });
+            }
+            return liste;
+        }
+    }
+}
---
+++ StokTakip.BLL/UrunService.cs
@@ -0,0 +1,53 @@
+﻿using StokTakip.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.BLL
+{
+    public class UrunService
+    {
+        VeriBaglantisi veritabani = new VeriBaglantisi();
+
+        public string GirisYap(int id, string sifre)
+        {
+            string rol = veritabani.KullaniciGirisKontrol(id, sifre);
+            return rol;
+        }
+        public void YeniUrunEkle(Urun yeniUrun)
+        {
+            veritabani.UrunEkle(yeniUrun);
+        }
+        public DataTable TumUrunleriGetir()
+        {
+            return veritabani.UrunleriListele();
+        }
+        public Urun SatisIcinUrunGetir(string barkod, int istenenAdet)
+        {
+            UrunDAO urunDao = new UrunDAO();
+            Urun urun = urunDao.BarkodlaUrunGetir(barkod);
+
+            if (urun == null)
+                throw new Exception("Ürün bulunamadı!");
+
+            if (urun.StokAdedi < istenenAdet)
+                throw new Exception("Stok yetersiz! Mevcut stok: " + urun.StokAdedi);
+
+            return urun;
+        }
+        public void StokDus(string barkod, int adet)
+        {
+            UrunDAO urunDao = new UrunDAO();
+            urunDao.StokGuncelle(barkod, adet);
+        }
+        public void SatisTamamla(string barkod, int adet)
+        {
+            UrunDAO urunDao = new UrunDAO();
+            urunDao.StokGuncelle(barkod, adet);
+        }
+    }
+}
